25cb69e051f20511b485a8abef1664a5968befef
Kirari Kirisaki <kirisaki124@yahoo.com>
2018-07-07 20:56:47 +0700

    upload image

--- modules/api/post/controller.js
+++ modules/api/post/controller.js
@@ -6,6 +6,7 @@ const createPost = ({title, description, userid, imageFile}) =>
         postModel
             .create({
                 image: fs.readFileSync(imageFile.path),
+                contentType: imageFile.mimetype,
                 title: title,
                 description: description,
                 createBy: userid
@@ -14,6 +15,58 @@ const createPost = ({title, description, userid, imageFile}) =>
             .catch(err => reject(err))
     })
 
+const getAllPosts = () => 
+    new Promise((resolve, reject) => {
+        postModel
+            .find({active: true})
+            .select("_id title image description")
+            .exec()
+            .then(data => {
+                resolve(data.map(img =>
+                    Object.assign({}, img._doc, {
+                        imageUrl: `/api/posts/${img._id}/data`
+                    })
+                ))
+            })
+            .catch(err => reject(err))
+    })
+
+const getPost = (id) =>
+    new Promise((resolve, reject) => {
+        postModel
+            .update({active: true})
+            .then(result => 
+                postModel
+                    .findOne({
+                        active: true,
+                        _id: id
+                    })
+                    .select("_id title description")
+                    .exec()
+            )
+            .then(data =>
+                resolve(
+                    Object.assign({}, data._doc, { imageUrl: `/api/posts/${id}/data`})
+                )
+            )
+    })
+
+const getImageData = id =>
+  new Promise((resolve, reject) => {
+    postModel
+      .findOne({
+        active: true,
+        _id: id
+      })
+      .select("image contentType")
+      .exec()
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
+
 module.exports = {
-    createPost
+    createPost,
+    getAllPosts,
+    getPost,
+    getImageData
 }
--- modules/api/post/router.js
+++ modules/api/post/router.js
@@ -5,18 +5,39 @@ const upload = multer({ dest: "uploads/" })
 
 const postController = require('./controller')
 
-router.post('/', upload.single("image"), (req, res) => {
-    req.body.userId = req.session.userInfo.id
+router.post('/', upload.single("image"), (req, res, next) => {
+    // req.body.userId = req.session.userInfo.id
     req.body.imageFile = req.file
 
     postController
         .createPost(req.body)
         .then(results => res.send(results))
-        .catch(err => console.err(err))
+        .catch(err => console.log(err))
 })
 
 router.get('/', (req, res) => {
-    res.send("hello")
+    postController
+        .getAllPosts()
+        .then(data => res.send(data))
+        .catch(err => console.log(err))
 })
+
+router.get('/:postId', (req, res) => {
+    postController
+        .getPost(req.params.postId)
+        .then(post => res.send(post))
+        .catch(err => console.log)
+})
+
+router.get('/:postId/data', (req, res) => {
+    postController
+        .getImageData(req.params.postId)
+        .then(data => {
+            res.contentType(data.contentType)
+            console.log(data)
+            res.send(data.image)
+        })
+})
+
 
 module.exports = router
